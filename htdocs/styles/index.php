44e4ff95bd30b7a1920a411ab8b5f78396114267
FunElephant <derek.ronske@gmail.com>
2015-06-26 13:45:16 -0500

    Added index.php.

---
+++ htdocs/styles/index.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<head>
+<link rel="stylesheet" type="text/css" href="profile.css">
+</head>
+	<div id="side">
+		<ul>	
+			<li><img src="http://i.imgur.com/MX3xQqE.png"></li>
+			<li><img src="http://i.imgur.com/J1Ag93s.png"></li>
+		</ul>
+	</div>
+	<div id="postsec">
+		<div class="post">
+			<p>hello</p>
+		</div>
+	</div>
+
+</body>
+</html>
