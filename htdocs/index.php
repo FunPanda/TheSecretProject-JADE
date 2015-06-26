492d5a3687e58d0ca2f27467a7dec207154d5347
FunElephant <derek.ronske@gmail.com>
2015-06-26 14:06:27 -0500

    Updated.

--- htdocs/index.php
+++ htdocs/index.php
@@ -9,7 +9,17 @@
 			<li><img src="http://i.imgur.com/J1Ag93s.png"></li>
 		</ul>
 	</div>
+	
+	<div id="infosec">
+		<h2>derek</h2>
+		<p>test info test test test</p>
+	</div>
+
+
 	<div id="postsec">
+		<div class="post">
+			<p>hello it is me derek ronske i am here today to talk about bjork bjork is a great human being who is really good at singing amongst other things and she is very cool</p>
+		</div>
 		<div class="post">
 			<p>hello</p>
 		</div>
