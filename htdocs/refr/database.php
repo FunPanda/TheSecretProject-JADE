35105a57e5ec58b830ff5971b6cf99d07c346a04
FunPanda <zreiphone@gmail.com>
2015-06-26 21:56:58 -0500

    Updating Refrences

--- htdocs/refr/database.php
+++ htdocs/refr/database.php
@@ -2,5 +2,8 @@
     error_reporting(0);
 
     //define CONSTANTS
-    define("DB_DSN","mysql:host=localhost;dbname=account_info"
+    define("DB_DSN","mysql:host=localhost;dbname=account_info");
+    define("DB_USRNAME","root");
+    define("DB_PASSWD","Jv4FsBTcUuEpdwAE");
+    //define("CLS_PATH", "handlers");
 ?>
